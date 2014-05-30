bb29c8682a27da0da7546dea00cb46391b58fbad
Jonas Thiel <jt@neopoly.de>
2014-05-30 13:44:54 +0200

    Plugin for link to users in wiki pages showing an avatar

---
+++ plugins/user_wiki_macro/init.rb
@@ -0,0 +1,17 @@
+Redmine::Plugin.register :user_wiki_macro do
+  name "User wiki macro"
+  author "Jonas Thiel"
+  description "Add a {{user(login_name)}} macro to the wiki engine"
+  version "0.0.1"
+  url "http://www.neopoly.de"
+  author_url "http://www.neopoly.de"
+
+  ActionDispatch::Callbacks.to_prepare do
+    require "user_wiki_macro"
+    require "user_wiki_macro_application_hooks"
+  end
+end
+
+ActionView::Base.class_eval do
+  include ActionView::Helpers::UserWikiMacroHelper
+end
