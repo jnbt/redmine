e964b9c8821ac4666bce8306090703782eb759c9
Jonas Thiel <jt@neopoly.de>
2014-05-30 12:00:56 +0200

    Implement a plugin handling authentication via HTTP basic authorization

---
+++ plugins/http_basic_authorization/init.rb
@@ -0,0 +1,22 @@
+Redmine::Plugin.register :http_basic_authorization do
+  name "Http Basic Authorization plugin"
+  author "Jonas Thiel"
+  description "This plugin uses HTTP basic authentication headers for login" \
+              " and IMPLICIT registration!"
+  version "0.0.1"
+  url "http://www.neopoly.de"
+  author_url "http://www.neopoly.de"
+
+  ActionDispatch::Callbacks.to_prepare do
+    require "http_basic_authorization"
+  end
+end
+
+RedmineApp::Application.config.after_initialize do
+  unless ApplicationController.include?(HttpBasicAuthorization::ApplicationControllerPatch)
+    ApplicationController.send(:include, HttpBasicAuthorization::ApplicationControllerPatch)
+  end
+  unless AccountController.include?(HttpBasicAuthorization::AccountControllerPatch)
+    AccountController.send(:include, HttpBasicAuthorization::AccountControllerPatch)
+  end
+end
