f4f9c24ee98cae04cfbded190d9f9c59c63d068d
Jonas Thiel <jt@neopoly.de>
2015-03-18 12:33:56 +0100

    Extract all plugins into gems

--- plugins/user_wiki_macro/lib/user_wiki_macro_application_hooks.rb
+++
@@ -1,5 +0,0 @@
-class UserWikiMacroApplicationHooks < Redmine::Hook::ViewListener
-  def view_layouts_base_html_head(_context)
-    stylesheet_link_tag "user_wiki_macro", plugin: "user_wiki_macro"
-  end
-end
